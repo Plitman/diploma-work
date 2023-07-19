9fa35481d1f86e73a258dbf7288b9aa5f2714e34
Plitman <139915673+Plitman@users.noreply.github.com>
2023-07-19 18:55:15 +0300

    Update user.py

--- user.py
+++ user.py
@@ -1,1 +1,22 @@
+class User:
+    def __init__(self, id, age, sex, city, relationship_status):
+        self.id = id
+        self.age = age
+        self.sex = sex
+        self.city = city
+        self.relationship_status = relationship_status
 
+    def get_id(self):
+        return self.id
+
+    def get_age(self):
+        return self.age
+
+    def get_sex(self):
+        return self.sex
+
+    def get_city(self):
+        return self.city
+
+    def get_relationship_status(self):
+        return self.relationship_status
